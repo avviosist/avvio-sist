50d18c6aa4c53c2f1af31728958e379d4a3cb708
avviosist <avviosistnexo@gmail.com>
2026-08-29 22:28:52 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "avvio-sist-v1";
+
+const ARCHIVOS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./logo.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ARCHIVOS);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
